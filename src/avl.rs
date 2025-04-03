24809159e14e3b451b835c84bca4456f3e85bc98
Max.Shel <maxs200099@gmail.com>
2025-04-03 21:31:51 +0700

    AVK set is done

--- src/avl.rs
+++ src/avl.rs
@@ -1,4 +1,4 @@
-use std::cmp::Ordering;
+use std::cmp::{Ordering};
 use std::io::Error;
 
 #[derive(Debug)]
@@ -7,7 +7,7 @@ pub struct AVLNode {
     pub right : Option<Box<AVLNode>>,
     pub key: String,
     pub value: String,
-    pub height: u32,
+    pub height: i32,
 }
 
 impl AVLNode {
@@ -17,7 +17,7 @@ impl AVLNode {
             right,
             key: key.to_string(),
             value: value.to_string(),
-            height: 0,
+            height: 1,
         }
     }
 }
@@ -28,42 +28,138 @@ pub struct AVLTree {
 }
 
 impl AVLTree {
-    pub fn new(key: &str, value: &str) -> AVLTree {
-        let node = AVLNode::new(key, value, None, None);
-        AVLTree {root: Some(Box::new(node))}
+    pub fn new() -> AVLTree {
+        AVLTree {root: None}
+    }
+
+    pub fn set(&mut self, key: &str, value: &str) -> Result<(), Error> {
+        self.root = Self::insert(self.root.take(), key, value);
+        Ok(())
+    }
+
+    fn insert(node: Option<Box<AVLNode>>, key: &str, value: &str) -> Option<Box<AVLNode>> {
+        match node {
+            Some(mut n) => {
+                match key.cmp(&n.key) {
+                    Ordering::Less => {
+                        n.left = Self::insert(n.left.take(), key, value);
+                    }
+                    Ordering::Greater => {
+                        n.right = Self::insert(n.right.take(), key, value);
+                    }
+                    Ordering::Equal => {
+                        n.value = value.to_string();
+                        return Some(n);
+                    }
+                }
+                Some(Self::balance(n))
+            }
+            None => Some(Box::new(AVLNode::new(key, value, None, None))),
+        }
     }
     
-    pub fn set(&mut self, key: &str, value: &str) -> Result<&AVLNode, Error> {
-        let mut current_tree = &mut self.root;
+    fn get_height(node: Option<&Box<AVLNode>>) -> i32 {
+        if let Some(n) = node {
+            return n.height;
+        } else {
+            0
+        }
+    }
+    
+    fn get_balance(node: Option<&Box<AVLNode>>) -> i32 {
+        if let Some(n) = node {
+            return Self::get_height(n.left.as_ref()) - Self::get_height(n.right.as_ref());
+        } else {
+            0
+        }
         
-        while let Some(current_node) = current_tree{
-            match key.cmp(&current_node.key) {
-                Ordering::Less => {
-                    if current_node.left.is_some() {
-                        current_tree = &mut current_node.left
-                    } else {
-                        current_node.left = Some(Box::new(AVLNode::new(key, value, None, None)));
-                        return Ok(current_node.left.as_deref().unwrap())
-                    }
-                },
-                Ordering::Greater => {
-                    if current_node.right.is_some() {
-                        current_tree = &mut current_node.right
-                    } else {
-                        current_node.right = Some(Box::new(AVLNode::new(key, value, None, None)));
-                        return Ok(current_node.right.as_deref().unwrap())
-                    }
-                },
-                Ordering::Equal => {
-                    current_node.value = value.to_string();
-                    return Ok(current_node)
-                }
-            }
+    }
+
+    fn right_rotate(mut node: Box<AVLNode>) -> Box<AVLNode> {
+        println!("Rotate right on node {}", node.key);
+        
+        let mut new_main_node = node.left.take().unwrap();
+        
+        node.left = new_main_node.right.take();
+        node.height = 1 + Self::get_height(node.left.as_ref()).max(Self::get_height(node.right.as_ref()));
+
+        new_main_node.right = Some(node);
+        new_main_node.height = 1 + Self::get_height(new_main_node.left.as_ref()).max(Self::get_height(new_main_node.right.as_ref()));
+        new_main_node
+    }
+
+    fn left_rotate(mut node: Box<AVLNode>) -> Box<AVLNode> {
+        println!("Rotate left on node {}", node.key);
+
+        let mut new_main_node = node.right.take().unwrap();
+        
+        node.right = new_main_node.left.take();
+        node.height = 1 + Self::get_height(node.right.as_ref()).max(Self::get_height(node.right.as_ref()));
+
+        new_main_node.left = Some(node);
+        new_main_node.height = 1 + Self::get_height(new_main_node.left.as_ref()).max(Self::get_height(new_main_node.right.as_ref()));
+        new_main_node
+
+    }
+    
+    fn balance(mut node: Box<AVLNode>) -> Box<AVLNode> {
+        node.height = 1 + Self::get_height(node.left.as_ref()).max(Self::get_height(node.right.as_ref()));
+        let balance = Self::get_balance(Some(&node));
+        
+        // Left Left
+        if balance > 1 && Self::get_balance(node.left.as_ref()) >= 0 {
+            return Self::right_rotate(node)
         }
     
-        Ok(current_tree.as_deref().unwrap())
+        // Left Right
+        if balance > 1 && Self::get_balance(node.left.as_ref()) < 0 {
+            node.left = Some(Self::left_rotate(node.left.unwrap()));
+            return Self::right_rotate(node)
+        }
+
+        // Right Right
+        if balance < -1 && Self::get_balance(node.right.as_ref()) <= 0 {
+            return Self::left_rotate(node);
+        }
         
+        // Right Left
+        if balance < -1 && Self::get_balance(node.right.as_ref()) > 0 {
+            node.right = Some(Self::right_rotate(node.right.unwrap()));
+            return Self::left_rotate(node)
+        }
+        
+        node
     }
+
+    // pub fn set(&mut self, node: Option<&mut AVLNode>, key: &str, value: &str) -> Result<&AVLNode, Error> {
+    //     let mut current_node = node.unwrap_or(&mut self.root.as_deref().unwrap());
+    //
+    //     match key.cmp(&current_node.key) {
+    //         Ordering::Less => {
+    //             if current_node.left.is_some() {
+    //                 self.set(&mut current_node.left, key, value)?;
+    //             } else {
+    //                 current_node.left = Some(Box::new(AVLNode::new(key, value, None, None)));
+    //                 return Ok(current_node.left)
+    //             }
+    //         },
+    //         Ordering::Greater => {
+    //             if current_node.right.is_some() {
+    //                 self.set(&mut current_node.right, key, value)?;
+    //             } else {
+    //                 current_node.right = Some(Box::new(AVLNode::new(key, value, None, None)));
+    //                 return Ok(current_node.right.as_deref().unwrap())
+    //             }
+    //         },
+    //         Ordering::Equal => {
+    //             current_node.value = value.to_string();
+    //             return Ok(current_node)
+    //         }
+    //     }
+    //
+    //     Ok(current_node)
+    //
+    // }
 
     // pub fn get(&mut self, key: &str,) -> Result<AVLNode, Error> {
     //     Ok()
--- tests/avl_tests.rs
+++ tests/avl_tests.rs
@@ -5,17 +5,53 @@ fn valid_tree() {
     let first_pair = ("qw", "first");
     let second_pair = ("q", "second");
     let third_pair = ("qwe", "third");
+    let fourth_pair = ("qwer", "fourth");
+    let fifth_pair = ("qwert", "fifth");
     
-    let mut tree = AVLTree::new(first_pair.0, first_pair.1);
-    assert_eq!(tree.root.as_deref().unwrap().key, first_pair.0);
-    assert_eq!(tree.root.as_deref().unwrap().value, first_pair.1);
-    
+    let mut tree = AVLTree::new();
+    tree.set(first_pair.0, first_pair.1).unwrap();
+    let root = tree.root.as_ref().unwrap();
+    assert_eq!(root.key, first_pair.0);
+    assert_eq!(root.value, first_pair.1);
+    assert_eq!(root.height, 1); 
+
+    // Value should be inserted to the left
     tree.set(second_pair.0, second_pair.1).unwrap();
-    
-    assert_eq!(tree.root.as_deref().unwrap().left.as_deref().unwrap().key, second_pair.0);
-    assert_eq!(tree.root.as_deref().unwrap().left.as_deref().unwrap().value, second_pair.1);
-    
+    let root = tree.root.as_ref().unwrap();
+    let root_left = root.left.as_deref().unwrap();
+    assert_eq!(root_left.key, second_pair.0);
+    assert_eq!(root_left.value, second_pair.1);
+    assert_eq!(root.height, 2);
+
+    // Value should be inserted to the right
     tree.set(third_pair.0, third_pair.1).unwrap();
-    assert_eq!(tree.root.as_deref().unwrap().right.as_deref().unwrap().key, third_pair.0);
-    assert_eq!(tree.root.as_deref().unwrap().right.as_deref().unwrap().value, third_pair.1);
+    let root = tree.root.as_ref().unwrap();
+    let root_right = root.right.as_deref().unwrap();
+    assert_eq!(root_right.key, third_pair.0);
+    assert_eq!(root_right.value, third_pair.1);
+    assert_eq!(root.height, 2);
+
+    // ReBalance... 
+    tree.set(fourth_pair.0, fourth_pair.1).unwrap();
+    tree.set(fifth_pair.0, fifth_pair.1).unwrap();
+    let root = tree.root.as_ref().unwrap();
+    assert_eq!(root.key, first_pair.0);
+    assert_eq!(root.height, 3);
+    
+    assert_eq!(root.left.as_deref().unwrap().key, second_pair.0);
+    assert_eq!(root.left.as_deref().unwrap().height, 1);
+    
+    let root_right = root.right.as_deref().unwrap();
+    assert_eq!(root_right.key, fourth_pair.0);
+    assert_eq!(root_right.height, 2);
+    
+    let root_right_left = root_right.left.as_deref().unwrap();
+    assert_eq!(root_right_left.key, third_pair.0);
+    assert_eq!(root_right_left.height, 1);
+
+    let root_right_right = root_right.right.as_deref().unwrap();
+    assert_eq!(root_right_right.key, fifth_pair.0);
+    assert_eq!(root_right_right.height, 1);
+    
+    
 }
